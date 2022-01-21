25859870dc38adfd07a73f4fa53834efa9421f10
Érick <ricksantosmatos@gmail.com>
2022-01-21 02:30:33 -0300

    Update telaAdicionaCarro.php

--- telaAdicionaCarro.php
+++ telaAdicionaCarro.php
@@ -7,35 +7,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Estacionamento</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
-
-
-<body>    
-    <div class="container">
+<body>
+    <div class="container" style="border: 4px solid black; padding: 0; margin-top: 5%;">
         <form action="cadastro.php" method="POST">
-            <div class="mb-3">
+            <div class="imagem-topo">
+                <h4 class="text-center text-white">
+                    <b>Estacionamento</b>
+                </h4>
+            </div>
+            <div class="mb-3 usuario" style="margin: 40px 0 40px 0; padding: 0 40px 0 40px;">
                 <label for="ImputPlaca" class="form-label">Placa</label>
                 <input type="text" class="form-control" name="id_placa" id="ImputPlaca">
             </div>
-            <div class="mb-3">
+            <div class="mb-3 usuario" style="margin: 40px 0 40px 0; padding: 0 40px 0 40px;">
                 <label for="ImputMarca" class="form-label">Marca</label>
                 <input type="text" class="form-control" name="marca" id="ImputMarca">
             </div>
-            <div class="mb-3">
+            <div class="mb-3 usuario" style="margin: 40px 0 40px 0; padding: 0 40px 0 40px;">
                 <label for="ImputModelo" class="form-label">Modelo</label>
                 <input type="text" class="form-control" name="modelo" id="ImputModelo">
             </div>
-            <div class="mb-3">
+            <div class="mb-3 usuario" style="margin: 40px 0 40px 0; padding: 0 40px 0 40px;">
                 <label for="ImputAno" class="form-label">Ano</label>
                 <input type="text" class="form-control" name="ano_modelo" id="ImputAno">
             </div>
-            <div class="mb-3">
+            <div class="mb-3 usuario" style="margin: 40px 0 40px 0; padding: 0 40px 0 40px;">
                 <label for="ImputCor" class="form-label">Cor</label>
                 <input type="text" class="form-control" name="cor" id="ImputCor">
             </div>
-            <a class="btn btn-outline-primary" href="estacionamento.html" role="button">Voltar</a>
-            <button type="submit" href="estacionamento.html" class="btn btn-primary">Cadastrar Carro</button>
+            <div class="container" style="margin: 40px 0 40px 0;">
+                <a class="botoes cor-gradiente text-white link" style="margin-right: 20px" href="estacionamento.html" role="button">Voltar</a>
+                <button type="submit" class="botoes cor-gradiente text-white link">Cadastrar Carro</button>
+            </div>
+
         </form>
     </div>
 
