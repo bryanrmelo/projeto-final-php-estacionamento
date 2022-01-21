704cbb5f9bb11da821cd5c597e331b1db8939e01
Bryan Melo <bryanrmelo@gmail.com>
2022-01-21 01:07:27 -0300

    Front-end

--- home_admin.php
+++ home_admin.php
@@ -8,17 +8,23 @@
     <title>Estacionamento</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <?php
-    include_once('estacionamentoDao_admin.php');
-    $listaCarros = buscaTodosCarros();
-    
-    foreach ($listaCarros as $value);        
+include_once('estacionamentoDao_admin.php');
+$listaCarros = buscaTodosCarros();
+
+foreach ($listaCarros as $value);
 ?>
- 
+
 <body>
-    <div class="container">
+    <div class="container" style="border: 4px solid black; padding: 0; margin-top: 5%;">
+        <div class="imagem-topo">
+            <h4 class="text-center text-white">
+                <b>Estacionamento</b>
+            </h4>
+        </div>
         <section>
             <table class="table table-striped">
                 <thead>
@@ -46,16 +52,17 @@
             </table>
         </section>
         <section>
-            <a class="btn btn-primary" href="telaAdicionaCarro_admin.php" role="button">Cadastrar Novo Veículo</a>
+            <a class="botoes cor-gradiente text-white" style="margin-left: 20px; text-decoration: none;" href="telaAdicionaCarro_admin.php" role="button">Cadastrar Novo Veículo</a>
             <br>
             <br>
-            <a class="btn btn-primary" href="logout.php"> Sair </class> </a> 
+            <a class="botoes cor-gradiente text-white" style="margin-left: 20px; text-decoration: none;" href="logout.php"> Sair </class> </a>
         </section>
         <section>
             <br>
             <br>
         </section>
     </div>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
+
 </html>
