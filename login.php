ffe1f0091d1f77aeb91be996ebb3d8ca9e82a45b
Bryan Melo <bryanrmelo@gmail.com>
2022-01-21 01:20:09 -0300

    Update correção login

--- login.php
+++ login.php
@@ -1,22 +1,21 @@
 <?php
 // print_r($_POST);
 
-$usuario = $_POST['userName'];
+$usuario = $_POST['admin'];
 $senha = $_POST['userPassword'];
 
 // Verificar login
-$loginValido = TRUE;
+$loginValido = $usuario == 'admin' && $senha == 'master' ? true : false;
 
 // configurar a session
-if ($loginValido) {
-
+if ($loginValido == 1) {
+    // print_r($loginValido);
     session_start();
     $_SESSION['nome_usuario'] = $usuario;
     $_SESSION['senha_usuario'] = $senha;
 
     header('Location:home_admin.php');
     die();
-
 } else {
     header('Location:estacionamento_admin.html');
 }
